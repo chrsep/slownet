473143fc0a6f1ef0a8383796ad547ed99d6ab026
Chrisando Pramudhita <chrsep@protonmail.com>
2021-03-27 11:57:30 +0700

    add some .env and some refactor.

---
+++ src/lib/leaflet.ts
@@ -0,0 +1,25 @@
+import { map, tileLayer } from "leaflet"
+
+export const setupMap = (mapContainer) => {
+  const myMap = map(mapContainer, {
+    center: [51.505, -0.09],
+    zoom: 13,
+    preferCanvas: true,
+  })
+
+  tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      // @ts-ignore
+      accessToken: import.meta.env.VITE_MAPBOX_ACCESS_KEY as string,
+    }
+  ).addTo(myMap)
+
+  return myMap
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,19 @@
 <script lang="ts">
   import { onMount, tick } from "svelte"
+  import { browser } from "$app/env"
 
   let mapContainer
 
   onMount(async () => {
     await tick()
-    const { map, tileLayer } = await import("leaflet")
 
-    const myMap = map(mapContainer, {
-      center: [51.505, -0.09],
-      zoom: 13,
-      preferCanvas: true
-    })
-
-    tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox/streets-v11",
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: import.meta.env.VITE_MAPBOX_ACCESS_KEY
-    }).addTo(myMap)
+    if (browser) {
+      const { setupMap } = await import("$lib/leaflet")
+      setupMap(mapContainer)
+    }
   })
 </script>
 
 <main>
-  <div class="w-screen h-screen" bind:this={mapContainer}></div>
+  <div class="w-screen h-screen" bind:this={mapContainer} />
 </main>
